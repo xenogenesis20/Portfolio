356202b089cc3654deb038d2dac1e5798d0a9937
xenogenesis20 <xenogenesis20@outlook.com>
2021-08-22 23:22:28 +0000

    its alive, almost

--- src/assets/githubprojects.js
+++ src/assets/githubprojects.js
@@ -1,4 +1,50 @@
+import gif from "./nv.gif";
+
 const PROJECTS = [
+	{
+		title: "Next Visit",
+		description: `Web app for users to track symptoms, vitals, medications and doctor visits.`,
+		image: "https://i.imgur.com/GRvazvP.png",
+		github: "https://github.com/xenogenesis20/Next-Visit-",
+		date: "Aug, 2021",
+		stack: "React, JS",
+		snipet: false,
+		favorite: true,
+		backend: false
+	},
+	{
+		title: "Next Visit back end",
+		description: `Backend for Next Visit. Made with Python, Flask, sqlAlchemy.`,
+		image: "https://i.imgur.com/3dHMQok.png",
+		github: "https://github.com/xenogenesis20/Next-Visit----Back-End-",
+		date: " Aug, 2021",
+		stack: "Python, Flask",
+		snipet: false,
+		favorite: true,
+		backend: true
+	},
+	{
+		title: "Bootstrap exercise",
+		description: `Exercise to learn about bootstrap components`,
+		image: "https://i.imgur.com/I2qCsah.png",
+		github: "https://github.com/xenogenesis20/Bootstrap-project",
+		date: "Apr, 2021",
+		stack: "HTML, CSS",
+		snipet: false,
+		favorite: false,
+		backend: false
+	},
+	{
+		title: "Dom  exercise",
+		description: `Exercise to learn about manipulating the DOM`,
+		image: "https://i.imgur.com/CeHmHYL.png",
+		github: "https://github.com/xenogenesis20/excuse--generator",
+		date: "Apr, 2021",
+		stack: "JS, CSS,",
+		snipet: true,
+		favorite: false,
+		backend: false
+	},
 	{
 		title: "Product page",
 		description: `Simple product page view`,
@@ -11,13 +57,24 @@ const PROJECTS = [
 		backend: false
 	},
 	{
-		title: "Bootstrap exercise",
-		description: `Exercise to learn about bootstrap components`,
-		image: "https://i.imgur.com/I2qCsah.png",
-		github: "https://github.com/xenogenesis20/Bootstrap-project",
-		date: "Apr, 2021",
-		stack: "HTML, CSS",
-		snipet: false,
+		title: "",
+		description: ``,
+		image: "",
+		github: "",
+		date: ", 2021",
+		stack: "JS, CSS,",
+		snipet: true,
+		favorite: false,
+		backend: false
+	},
+	{
+		title: "",
+		description: ``,
+		image: "",
+		github: "",
+		date: ", 2021",
+		stack: "JS, CSS,",
+		snipet: true,
 		favorite: false,
 		backend: false
 	}
