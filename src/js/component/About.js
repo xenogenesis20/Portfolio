129aadf3d95f9ef56af41d34164feb8655319697
xenogenesis20 <xenogenesis20@outlook.com>
2021-08-24 00:07:14 +0000

    base content laid out, themes next

--- src/js/component/About.js
+++ src/js/component/About.js
@@ -1,18 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "..//../styles/index.scss";
+import { Col, Row } from "react-bootstrap";
 
 const About = props => {
 	return (
 		<section className="height100vh">
-			<h1>About</h1>
-			<p>
-				Hey there! <br />I am glad you made it this far. Let me tell you a little bit about myself. My name is
-				Maikel Martinez , I was born in Cuba and my family decided to make the trip to the United States in
-				1994. Via boat mind you, I&apos;ve been told the trip was crazy! I have been working as a medical
-				assistant for the past 7 years in a pediatric neurology office and I have enjoyed it but I have found my
-				passion in coding. I began with the basics and then moved onto more advanced topics which you
-			</p>
+			<Row>
+				<Col>
+					<h1>About</h1>
+				</Col>
+			</Row>
+			<Row className="overflow-auto">
+				<Col>
+					<p>
+						Hey there! <br />I am glad you made it this far. Let me tell you a little bit about myself. My
+						name is Maikel Martinez , I was born in Cuba and my family decided to make the trip to the
+						United States in 1994. Via boat mind you, I&apos;ve been told the trip was crazy! I have been
+						working as a medical assistant for the past 7 years in a pediatric neurology office and I have
+						enjoyed it but I have found my passion in coding. I began with the basics and then moved onto
+						more advanced topics which you might have read about above in the skills section. I enjoy taking
+						on difficult problems and finding efficient scalable solutions. There is always something new to
+						learn in the world of web development and I dip my toes into every pool information I find.
+					</p>
+				</Col>
+			</Row>
 		</section>
 	);
 };
--- src/js/component/Contact.js
+++ src/js/component/Contact.js
@@ -1,16 +1,57 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "..//../styles/index.scss";
+import { Col, Row, Card } from "react-bootstrap";
 
 const Contact = props => {
 	return (
 		<section className="height100vh">
-			<h1>Contact</h1>
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus iusto repudiandae aperiam non rem
-				asperiores qui nemo dicta quibusdam dolore voluptates, possimus corporis est eligendi, doloremque
-				nostrum soluta provident. Magni!
-			</p>
+			<Row>
+				<Col>
+					<h1 className="blinking">Let&apos;s talk</h1>
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<p>
+						<strong>Congratulations!</strong> <br />
+						You have made it to the most important area of my website.
+					</p>
+				</Col>
+			</Row>
+			<Row>
+				<Col className="d-flex justify-content-center">
+					<Card className="mb-2" style={{ width: "18rem" }}>
+						<Card.Body>
+							<Card.Title>Email</Card.Title>
+							<Card.Subtitle className="mb-2 text-muted">Send one, its free :)</Card.Subtitle>
+							<Card.Text>Xenogenesis20@outlook.com</Card.Text>
+							<Card.Link href="#">Card Link</Card.Link>
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col className="d-flex justify-content-center">
+					<Card className="mb-2" style={{ width: "18rem" }}>
+						<Card.Body>
+							<Card.Title>Links</Card.Title>
+							<Card.Subtitle className="mb-2 text-muted">Click one, you know you want to</Card.Subtitle>
+							<Card.Text>Some Social Media links </Card.Text>
+							<Card.Link className="icon-link" target="_blank" href="https://github.com/xenogenesis20">
+								<i className="fab fa-github" />
+							</Card.Link>
+							<Card.Link
+								className="icon-link"
+								target="_blank"
+								href="https://www.linkedin.com/in/maikel-martinez-445115205/">
+								<i className="fab fa-linkedin-in" />
+							</Card.Link>
+							<Card.Link className="icon-link" target="_blank" href="https://twitter.com/FWD_Problems">
+								<i className="fab fa-twitter" />
+							</Card.Link>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
 		</section>
 	);
 };
